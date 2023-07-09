ee1f0683768532f68ff3cc5c88567e3f74c60a5b
UDAY-CHAITANYA-G <127864450+UDAY-CHAITANYA-G@users.noreply.github.com>
2023-07-09 20:07:05 +0530

    clone webiste completed

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuIcon= document.querySelector(".bx-menu");
+let sidebar=document.querySelector(".sidebar");
+let container=document.querySelector(".container")
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container")
+}
